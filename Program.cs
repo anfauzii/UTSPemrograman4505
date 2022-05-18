33faa80be67efc3d0879dbe8eb7f181948b3272e
Ahmad Nur Fauzi <ahmadnurfauzi1704@gmail.com>
2022-05-18 16:19:16 +0700

    Source Code UTSPemrograman4505

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS_21114505
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama, course;
+            int nik;
+
+            Dosen utama = new Dosen();
+            utama.id = 21;
+            utama.nama = "Kamarudin, M.Kom";
+            utama.Nik = 212022;
+            utama.gender = "Laki-laki";
+            utama.course = "Pemrograman";
+            Console.WriteLine("===== DATA DOSEN DEFAULT =====");
+            Console.WriteLine();
+            utama.dosen();
+            Console.WriteLine();
+            Console.WriteLine("===== INPUT DATA DOSEN =====");
+            Console.WriteLine();
+            Console.Write("Masukkan Nama Dosen \t\t: ");
+            nama = Console.ReadLine();
+            Console.Write("Masukkan NIK Dosen \t\t: ");
+            nik = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Masukkan Mata Kuliah Dosen \t: ");
+            course = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("===== DATA DOSEN YANG SUDAH DIINPUTKAN =====");
+            Console.WriteLine();
+            Dosen.attdosen(nama, nik);
+            Dosen.addcourse(course);
+
+            Console.ReadKey();
+        }
+    }
+}
